50c0e8d045cac36cbb231bcc88efa6631cfa9973
Daniil <ksusonic@yandex.ru>
2025-04-10 22:45:27 +0300

    Add questions

--- travel_agent/pipeline/generate_questions.py
+++ travel_agent/pipeline/generate_questions.py
@@ -3,68 +3,94 @@ import yaml
 import logging
 import pandas as pd
 from pathlib import Path
-from tqdm import trange
+from tqdm import tqdm, trange
 
 
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
 
+question_col = "question"
+expected_places_col = "expected_places"
+
+question_types = {"rubric", "rating", "address", "keyword", "rating_and_rubric"}
+
 
 def generate_questions_df(df, params) -> pd.DataFrame:
+    assert question_types == set(params["n_questions"].keys())
+
     random.seed(params["seed"])
 
     unique_rubrics = set()
     for row in df["rubrics"].dropna():
-        unique_rubrics.update(r.strip() for r in row.split(";"))
+        unique_rubrics.update(r.strip().lower() for r in row.split(";"))
+    unique_rubrics = sorted(
+        unique_rubrics
+    )  # обратно в list, чтобы избежать рандомного порядка
 
-    addresses = df["address"].dropna().unique()
+    addresses = df["address"].dropna().unique().tolist()
+    ratings = [i for i in range(1, 6)]
 
     logger.info("Unique rubrics: %d", len(unique_rubrics))
     logger.info("Unique addresses: %d", len(addresses))
 
     questions_data = []
 
-    for _ in trange(params["n_questions"], desc="Processing questions", leave=False):
-        question_type = random.choice(
-            ["rubric", "rating", "address", "keyword", "combined"]
-        )
+    for question_type, count in tqdm(params["n_questions"].items(), leave=False):
+        for _ in trange(count, desc=f"Question type {question_type}", leave=False):
+            if question_type == "rubric":
+                rubric = random.choice(unique_rubrics)
+                unique_rubrics.remove(rubric)
 
-        if question_type == "rubric":
-            rubric = random.choice(list(unique_rubrics))
-            q = f"Какие места в Москве относятся к категории «{rubric}»?"
-            expected = (
-                df[df["rubrics"].str.contains(rubric, na=False)]["name_ru"]
-                .unique()
-                .tolist()
-            )
+                q = f"Какие интересные места из категории {rubric} ты знаешь?"
+                expected = (
+                    df[df["rubrics"].str.contains(rubric, na=False)]["name_ru"]
+                    .unique()
+                    .tolist()
+                )
 
-        elif question_type == "rating":
-            q = "Какие заведения в Москве имеют рейтинг 5?"
-            expected = df[df["rating"] >= 5.0]["name_ru"].unique().tolist()
+            elif question_type == "rating":
+                rating = random.choice(ratings)
+                ratings.remove(rating)
 
-        elif question_type == "address":
-            addr = random.choice(addresses)
-            q = f"Что интересного можно найти по адресу: {addr}?"
-            expected = df[df["address"] == addr]["name_ru"].unique().tolist()
+                if len(ratings) == 0:
+                    continue
 
-        elif question_type == "keyword":
-            kw = random.choice(params["keywords"])
-            q = f"Где в Москве в отзывах упоминается «{kw}»?"
-            expected = (
-                df[df["text"].str.contains(kw, case=False, na=False)]["name_ru"]
-                .unique()
-                .tolist()
-            )
+                q = f"Покажи заведения с рейтингом {rating} и больше."
+                expected = df[df["rating"] >= rating]["name_ru"].unique().tolist()
 
-        elif question_type == "combined":
-            rubric = random.choice(list(unique_rubrics))
-            q = f"Найди заведения в Москве с рейтингом 5 и рубрикой «{rubric}»."
-            subset = df[
-                (df["rating"] >= 5.0) & (df["rubrics"].str.contains(rubric, na=False))
-            ]
-            expected = subset["name_ru"].unique().tolist()
+            elif question_type == "address":
+                addr = random.choice(addresses)
+                addresses.remove(addr)
 
-        questions_data.append({"question": q, "expected_places": expected})
+                q = f"Какие заведения есть по адресу {addr}?"
+                expected = df[df["address"] == addr]["name_ru"].unique().tolist()
+
+            elif question_type == "keyword":
+                kw = random.choice(params["keywords"])
+                params["keywords"].remove(kw)
+
+                q = f"Где в отзывах упоминается «{kw}»?"
+                expected = (
+                    df[df["text"].str.contains(kw, case=False, na=False)]["name_ru"]
+                    .unique()
+                    .tolist()
+                )
+
+            elif question_type == "rating_and_rubric":
+                rating = random.randint(1, 5)
+                rubric = random.choice(list(unique_rubrics)).lower()
+
+                q = f"Найди заведения {rubric} с рейтингом не ниже {rating}."
+                subset = df[
+                    (df["rating"] >= rating)
+                    & (df["rubrics"].str.contains(rubric, na=False))
+                ]
+                expected = subset["name_ru"].unique().tolist()
+
+            else:
+                logger.error("unknown question_type: %s", question_type)
+
+            questions_data.append({question_col: q, expected_places_col: expected})
 
     return pd.DataFrame(questions_data)
 
@@ -84,7 +110,12 @@ def main():
     output_path = f"{params['out_dir']}/{params['out_filename']}"
 
     questions_df.to_csv(output_path, index=False)
-    logger.info("Saved questions to %s", output_path)
+    logger.info("Saved questions to %s, logging result:", output_path)
+
+    logger.info(
+        "Logging result questions: \n%s",
+        "\n".join(questions_df[question_col].to_list()),
+    )
 
 
 if __name__ == "__main__":
