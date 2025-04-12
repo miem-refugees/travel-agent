de9445c9c90e3e2ea146cbec97879c1881352d63
Daniil <ksusonic@yandex.ru>
2025-04-12 16:38:03 +0300

    Add loguru and pydantic (#7)

--- travel_agent/pipeline/generate_questions.py
+++ travel_agent/pipeline/generate_questions.py
@@ -1,13 +1,11 @@
 import random
+from pydantic import BaseModel
 import yaml
-import logging
 import pandas as pd
 from pathlib import Path
+from loguru import logger
 from tqdm import tqdm, trange
 
-
-logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
-logger = logging.getLogger(__name__)
 
 question_col = "question"
 expected_places_col = "expected_places"
@@ -15,10 +13,19 @@ expected_places_col = "expected_places"
 question_types = {"rubric", "rating", "address", "keyword", "rating_and_rubric"}
 
 
-def generate_questions_df(df, params) -> pd.DataFrame:
-    assert question_types == set(params["n_questions"].keys())
+class Params(BaseModel):
+    seed: int
+    dataset: str
+    out_dir: str
+    out_filename: str
+    n_questions: dict[str, int]
+    keywords: list[str]
 
-    random.seed(params["seed"])
+
+def generate_questions_df(df, params) -> pd.DataFrame:
+    assert question_types == set(params.n_questions.keys())
+
+    random.seed(params.seed)
 
     unique_rubrics = set()
     for row in df["rubrics"].dropna():
@@ -27,21 +34,21 @@ def generate_questions_df(df, params) -> pd.DataFrame:
         unique_rubrics
     )  # обратно в list, чтобы избежать рандомного порядка
 
-    addresses = df["address"].dropna().unique().tolist()
+    addresses = sorted(df["address"].dropna().unique().tolist())
     ratings = [i for i in range(1, 6)]
 
-    logger.info("Unique rubrics: %d", len(unique_rubrics))
-    logger.info("Unique addresses: %d", len(addresses))
+    logger.info("Unique rubrics: {}", len(unique_rubrics))
+    logger.info("Unique addresses: {}", len(addresses))
 
     questions_data = []
 
-    for question_type, count in tqdm(params["n_questions"].items(), leave=False):
+    for question_type, count in tqdm(params.n_questions.items(), leave=False):
         for _ in trange(count, desc=f"Question type {question_type}", leave=False):
             if question_type == "rubric":
                 rubric = random.choice(unique_rubrics)
                 unique_rubrics.remove(rubric)
 
-                q = f"Какие интересные места из категории {rubric} ты знаешь?"
+                q = f"Какие {rubric} ты знаешь?"
                 expected = (
                     df[df["rubrics"].str.contains(rubric, na=False)]["name_ru"]
                     .unique()
@@ -66,8 +73,8 @@ def generate_questions_df(df, params) -> pd.DataFrame:
                 expected = df[df["address"] == addr]["name_ru"].unique().tolist()
 
             elif question_type == "keyword":
-                kw = random.choice(params["keywords"])
-                params["keywords"].remove(kw)
+                kw = random.choice(params.keywords)
+                params.keywords.remove(kw)
 
                 q = f"Где в отзывах упоминается «{kw}»?"
                 expected = (
@@ -78,7 +85,7 @@ def generate_questions_df(df, params) -> pd.DataFrame:
 
             elif question_type == "rating_and_rubric":
                 rating = random.randint(1, 5)
-                rubric = random.choice(list(unique_rubrics)).lower()
+                rubric = random.choice(unique_rubrics).lower()
 
                 q = f"Найди заведения {rubric} с рейтингом не ниже {rating}."
                 subset = df[
@@ -88,7 +95,8 @@ def generate_questions_df(df, params) -> pd.DataFrame:
                 expected = subset["name_ru"].unique().tolist()
 
             else:
-                logger.error("unknown question_type: %s", question_type)
+                logger.error("unknown question_type: {}", question_type)
+                continue
 
             questions_data.append({question_col: q, expected_places_col: expected})
 
@@ -96,25 +104,26 @@ def generate_questions_df(df, params) -> pd.DataFrame:
 
 
 def main():
-    params = yaml.safe_load(open("params.yaml"))["generate_questions"]
-    logger.debug("Loaded params: %s", params)
+    params = Params.model_validate(
+        yaml.safe_load(open("params.yaml"))["generate_questions"]
+    )
+    logger.debug("Loaded params: {}", params)
 
-    df = pd.read_csv(params["dataset"]).dropna()
-    logger.info("Read dataset from %s", params["dataset"])
+    df = pd.read_csv(params.dataset).dropna()
+    logger.info("Read dataset from {}", params.dataset)
 
     questions_df = generate_questions_df(df, params)
 
-    out_dir = Path(params["out_dir"])
+    out_dir = Path(params.out_dir)
     out_dir.mkdir(parents=True, exist_ok=True)
 
-    output_path = f"{params['out_dir']}/{params['out_filename']}"
+    output_path = f"{params.out_dir}/{params.out_filename}"
 
     questions_df.to_csv(output_path, index=False)
-    logger.info("Saved questions to %s, logging result:", output_path)
+    logger.info("Saved questions to {}", output_path)
 
     logger.info(
-        "Logging result questions: \n%s",
-        "\n".join(questions_df[question_col].to_list()),
+        "Logging result: \n- {}", "\n- ".join(questions_df[question_col].to_list())
     )
 
 
