2af0e6cdf504c6436baae0438b2f137db50ce4ff
searayeah <insslava@gmail.com>
2025-04-12 17:53:02 +0300

    123

---
+++ travel_agent/utils.py
@@ -0,0 +1,17 @@
+import random
+import os
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+
+def seed_everything(seed: int, pytorch_init: bool = True) -> None:
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    if pytorch_init is True:
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
