bf174ff6bb16b15be5725c90eb49fd9ef3668c16
searayeah <insslava@gmail.com>
2025-04-15 23:12:02 +0300

    Added em beddings

--- travel_agent/retrieval/bench/embedding.py
+++ travel_agent/retrieval/bench/embedding.py
@@ -3,17 +3,11 @@ import torch
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.metrics import precision_score, recall_score
 import numpy as np
 from tqdm import tqdm
-from qdrant_client import QdrantClient
-import pandas as pd
-from sentence_transformers import SentenceTransformer
-import pandas as pd
 from pathlib import Path
 from travel_agent.utils import seed_everything
 from loguru import logger
-import torch
 
 MODELS = [
     "DeepPavlov/rubert-base-cased-sentence",
@@ -82,8 +76,8 @@ def benchmark_similarity(
     query_col: str,
     embedding_col: str,
     model: SentenceTransformer,
-    k: int = 10,
-) -> pd.DataFrame:
+    ks: list[int] = [10],
+) -> dict[int, float]:
     if (
         doc_col not in df.columns
         or query_col not in df.columns
@@ -96,59 +90,66 @@ def benchmark_similarity(
             f"DataFrame must contain '{doc_col}', '{query_col}' and '{embedding_col}' columns"
         )
 
-    ap_scores = []
-
     doc_embeddings = np.array(df[embedding_col].tolist())
+    results = {}
 
-    for query in df[query_col].unique():
-        logger.debug(f"Processing query: {query}")
-        query_embedding = model.encode([query], convert_to_numpy=True)
+    for k in ks:
+        ap_scores = []
 
-        similarities = cosine_similarity(query_embedding, doc_embeddings)[0]
+        for query in df[query_col].unique():
+            logger.debug(f"Processing query: {query}")
+            query_embedding = model.encode([query], convert_to_numpy=True)
 
-        top_k_indices = np.argsort(similarities)[::-1][:k]
-        top_k_types = df.iloc[top_k_indices][query_col].tolist()
+            similarities = cosine_similarity(query_embedding, doc_embeddings)[0]
 
-        for i in range(3):
-            logger.debug(
-                f"Top {i} docs for query: {df.iloc[top_k_indices][doc_col].tolist()[i]}"
-            )
+            top_k_indices = np.argsort(similarities)[::-1][:k]
+            top_k_types = df.iloc[top_k_indices][query_col].tolist()
 
-        relevant_list = [1 if t == query else 0 for t in top_k_types]
-        ap_at_k = average_precision_at_k(relevant_list, k)
-        ap_scores.append(ap_at_k)
+            for i in range(min(3, k)):
+                logger.debug(
+                    f"Top {i + 1} doc for query '{query}': {df.iloc[top_k_indices][doc_col].tolist()[i]}"
+                )
 
-    map_k = np.mean(ap_scores)
+            relevant_list = [1 if t == query else 0 for t in top_k_types]
+            ap_at_k = average_precision_at_k(relevant_list, k)
+            ap_scores.append(ap_at_k)
 
-    return map_k
+        map_k = np.mean(ap_scores)
+        results[k] = map_k
+
+    return results
 
 
 if __name__ == "__main__":
     import sys
+
     logger.remove()
-    logger.add(sys.stdout, level="INFO") 
+    logger.add(sys.stdout, level="INFO")
+
     seed = 42
     seed_everything(seed)
 
     doc_col = "text"
-    query_col = "rubrics"
+    # query_col = "question"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    k = 10
+    k = [1, 3, 5, 10, 20]
+
+    dataset_name = "moskva"
 
     embeddings_path = (
-        Path("data") / "embedding" / "geo-reviews-dataset-2023-with-embeddings.parquet"
+        Path("data") / "embedding" / f"{dataset_name}-with-embeddings.parquet"
     )
     if embeddings_path.exists():
         logger.info(f"Loading embeddings from {str(embeddings_path)}")
         df = pd.read_parquet(embeddings_path)
     else:
         logger.info(f"Existing {str(embeddings_path)} not found, using raw")
-        df_path = Path("data") / "raw" / "geo-reviews-dataset-2023.csv"
-        df = pd.read_csv(df_path).sample(1000)
+        df_path = Path("data") / "prepared" / f"{dataset_name}.csv"
+        df = pd.read_csv(df_path)
         df[doc_col] = df[doc_col].apply(preprocess_text)
-        df[query_col] = df[query_col].apply(preprocess_text)
+        # df[query_col] = df[query_col].apply(preprocess_text)
 
-    results = {}
+    # results = defaultdict(dict)
 
     for model_name in MODELS:
         logger.info(f"Generating embeddings using {model_name}")
@@ -160,22 +161,24 @@ if __name__ == "__main__":
 
         logger.info(f"Benchmarking model: {model_name}")
 
-        map_k = benchmark_similarity(
-            df=df,
-            doc_col=doc_col,
-            query_col=query_col,
-            embedding_col=embedding_col,
-            model=model,
-            k=k,
-        )
+        # map_k = benchmark_similarity(
+        #     df=df,
+        #     doc_col=doc_col,
+        #     query_col=query_col,
+        #     embedding_col=embedding_col,
+        #     model=model,
+        #     ks=k,
+        # )
 
-        results[model_name] = map_k
+        # for k_val, score in map_k.items():
+        #     results[model_name][f"map@{k_val}"] = score
         torch.cuda.empty_cache()
 
     embeddings_path.parent.mkdir(parents=True, exist_ok=True)
     df.to_parquet(embeddings_path, index=False)
 
-    results_df = pd.DataFrame(list(results.items()), columns=["model", f"map@{k}"])
-    results_path = Path("data") / "embedding" / "bench_results.csv"
-    results_path.parent.mkdir(parents=True, exist_ok=True)
-    results_df.to_csv(results_path, index=False)
+    # results_df = pd.DataFrame.from_dict(results, orient="index").reset_index()
+    # results_df = results_df.rename(columns={"index": "model"})
+    # results_path = Path("data") / "embedding" / "bench_results.csv"
+    # results_path.parent.mkdir(parents=True, exist_ok=True)
+    # results_df.to_csv(results_path, index=False)
--- travel_agent/utils.py
+++ travel_agent/utils.py
@@ -2,7 +2,6 @@ import random
 import os
 import numpy as np
 import torch
-from torch.utils.data import DataLoader
 
 
 def seed_everything(seed: int, pytorch_init: bool = True) -> None:
