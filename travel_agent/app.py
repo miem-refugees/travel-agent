6b2178a77fd96f5400d415153df775e29eeaacb1
Daniil <ksusonic@yandex.ru>
2025-05-09 19:25:30 +0300

    feat: duckduckgo search

--- travel_agent/app.py
+++ travel_agent/app.py
@@ -1,26 +1,35 @@
 from loguru import logger
-from smolagents import LiteLLMModel, ToolCallingAgent
+from smolagents import (
+    DuckDuckGoSearchTool,
+    LiteLLMModel,
+    MultiStepAgent,
+    ToolCallingAgent,
+    VisitWebpageTool,
+)
 
 from travel_agent.retrieval.smolagents.tool import GetExistingAvailableRubricsTool, TravelReviewQueryTool
 from travel_agent.ui.gradio import TravelGradioUI
 
 
 @logger.catch
-def init_agent(model_name: str):
+def init_agent(deepseek: bool) -> MultiStepAgent:
     logger.info("Init app dependencies...")
 
     from travel_agent.qdrant import client as qdrant_client
 
-    # To support llm via api uncomment:
-    # llm = InferenceClientModel("mistralai/Mistral-Small-3.1-24B-Instruct-2503")
+    if deepseek:
+        logger.info("Using Deepseek as LLM")
+        llm = LiteLLMModel(model_id="deepseek/deepseek-chat")
+    else:
+        model_name = "hf.co/IlyaGusev/saiga_nemo_12b_gguf:Q4_0"
 
-    llm = LiteLLMModel(
-        model_id=f"ollama_chat/{model_name}",
-        api_base="http://127.0.0.1:11434",
-        num_ctx=8192,
-    )
+        logger.info("Using ollama {} as LLM", model_name)
 
-    llm.create_client()
+        llm = LiteLLMModel(
+            model_id=f"ollama_chat/{model_name}",
+            api_base="http://127.0.0.1:11434",
+            num_ctx=8192,
+        )
 
     return ToolCallingAgent(
         model=llm,
@@ -31,6 +40,8 @@ def init_agent(model_name: str):
                 qdrant_client,
                 "moskva_intfloat_multilingual_e5_base",
             ),
+            DuckDuckGoSearchTool(),
+            VisitWebpageTool(),
         ],
         max_steps=7,
         verbosity_level=1,
@@ -38,11 +49,11 @@ def init_agent(model_name: str):
     )
 
 
-if __name__ == "__main__":
+def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, default="hf.co/IlyaGusev/saiga_nemo_12b_gguf:Q4_0")
+    parser.add_argument("--deepseek", type=bool, default=False)
     parser.add_argument("--tracing", type=bool, default=False)
     parser.add_argument("--share", type=bool, default=False)
     args = parser.parse_args()
@@ -56,11 +67,15 @@ if __name__ == "__main__":
         # automatically captures any smolagents calls as traces
         SmolagentsInstrumentor().instrument(tracer_provider=tracer_provider)
     else:
-        logger.info("Tracing disabled (launthing with --tracing flag)")
+        logger.debug("Tracing disabled (launthing with --tracing flag)")
 
-    agent = init_agent(args.model_name)
+    agent = init_agent(args.deepseek)
     if agent is None:
         logger.error("Agent initialization failed")
         exit(1)
 
     TravelGradioUI(agent).launch(share=args.share)
+
+
+if __name__ == "__main__":
+    main()
