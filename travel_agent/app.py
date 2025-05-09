8346e8a791442206bf74be1469452dc278e621db
Daniil <ksusonic@yandex.ru>
2025-05-09 20:35:55 +0300

    feat: duckduckgo search tool (#34)

--- travel_agent/app.py
+++ travel_agent/app.py
@@ -1,26 +1,39 @@
+import os
+
 from loguru import logger
-from smolagents import LiteLLMModel, ToolCallingAgent
+from smolagents import (
+    DuckDuckGoSearchTool,
+    LiteLLMModel,
+    ToolCallingAgent,
+    VisitWebpageTool,
+)
 
 from travel_agent.retrieval.smolagents.tool import GetExistingAvailableRubricsTool, TravelReviewQueryTool
 from travel_agent.ui.gradio import TravelGradioUI
 
 
 @logger.catch
-def init_agent(model_name: str):
+def init_agent(ollama: bool):
     logger.info("Init app dependencies...")
 
     from travel_agent.qdrant import client as qdrant_client
 
-    # To support llm via api uncomment:
-    # llm = InferenceClientModel("mistralai/Mistral-Small-3.1-24B-Instruct-2503")
+    if ollama:
+        model_name = "hf.co/IlyaGusev/saiga_nemo_12b_gguf:Q4_0"
 
-    llm = LiteLLMModel(
-        model_id=f"ollama_chat/{model_name}",
-        api_base="http://127.0.0.1:11434",
-        num_ctx=8192,
-    )
+        logger.info("Using ollama {} as LLM", model_name)
 
-    llm.create_client()
+        llm = LiteLLMModel(
+            model_id=f"ollama_chat/{model_name}",
+            api_base="http://127.0.0.1:11434",
+            num_ctx=8192,
+        )
+    elif os.getenv("DEEPSEEK_API_KEY"):
+        logger.info("Using Deepseek as LLM")
+        llm = LiteLLMModel(model_id="deepseek/deepseek-chat")
+    else:
+        logger.error("No LLM model info provided")
+        return
 
     return ToolCallingAgent(
         model=llm,
@@ -31,36 +44,48 @@ def init_agent(model_name: str):
                 qdrant_client,
                 "moskva_intfloat_multilingual_e5_base",
             ),
+            DuckDuckGoSearchTool(),
+            VisitWebpageTool(),
         ],
         max_steps=7,
         verbosity_level=1,
-        planning_interval=3,
+        planning_interval=5,
     )
 
 
-if __name__ == "__main__":
+def try_setup_langfuse_tracing():
+    if not os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT"):
+        logger.info("Tracing disabled (no OTEL env vars)")
+        return
+
+    from openinference.instrumentation.smolagents import SmolagentsInstrumentor
+    from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+
+    trace_provider = TracerProvider()
+    trace_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter()))
+
+    SmolagentsInstrumentor().instrument(tracer_provider=trace_provider)
+
+
+def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, default="hf.co/IlyaGusev/saiga_nemo_12b_gguf:Q4_0")
-    parser.add_argument("--tracing", type=bool, default=False)
+    parser.add_argument("--ollama", type=bool, default=False)
     parser.add_argument("--share", type=bool, default=False)
     args = parser.parse_args()
 
-    if args.tracing:
-        from openinference.instrumentation.smolagents import SmolagentsInstrumentor
-        from phoenix.otel import register
+    try_setup_langfuse_tracing()
 
-        # creates a tracer provider to capture OTEL traces
-        tracer_provider = register(project_name="travel-agent-local", verbose=False)
-        # automatically captures any smolagents calls as traces
-        SmolagentsInstrumentor().instrument(tracer_provider=tracer_provider)
-    else:
-        logger.info("Tracing disabled (launthing with --tracing flag)")
-
-    agent = init_agent(args.model_name)
+    agent = init_agent(args.ollama)
     if agent is None:
         logger.error("Agent initialization failed")
         exit(1)
 
     TravelGradioUI(agent).launch(share=args.share)
+
+
+if __name__ == "__main__":
+    main()
